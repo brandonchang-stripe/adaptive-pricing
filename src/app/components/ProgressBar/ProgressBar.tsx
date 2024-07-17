df9ffd8aa3048c2cb16e82399e0dd10cbff4e42b
Brandon Chang <brandonchang@stripe.com>
2024-07-17 15:01:24 -0400

    progress bar

--- src/app/components/ProgressBar/ProgressBar.tsx
+++ src/app/components/ProgressBar/ProgressBar.tsx
@@ -1,8 +1,9 @@
 import styles from "./ProgressBar.module.css";
 import { useAppStore, useCurrentCountry } from "@/app/store";
 import Frame from "../Frame/Frame";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 import { stepEase } from "@/app/util/stepEase";
+import { useEffect } from "react";
 
 export default function ProgressBar() {
   const currentItems = useCurrentCountry()?.items || [];
@@ -11,30 +12,43 @@ export default function ProgressBar() {
   return (
     <Frame label="Progress" position="progress-bar" type="simple" allowDrag>
       <div className={styles.progress}>
-        <div className={styles.text}>
-          <b>Items left:</b>
-        </div>
+        <div className={styles.text}>Items left</div>
         <div className={styles.progressContainer}>
           <AnimatePresence initial={false} mode="popLayout">
             {currentItems
               .filter((_, index) => index >= itemIndex)
               .map((item) => (
-                <motion.div
-                  key={item.type}
-                  animate={{ scale: 1, transition: { duration: 0.3, ease: stepEase(4) } }}
-                  exit={{
-                    scale: 0,
-                    transition: { duration: 0.3, ease: stepEase(3) },
-                  }}
-                  layout
-                  className={styles.item}
-                >
-                  +
-                </motion.div>
+                <Item key={item.type} item={item} />
               ))}
           </AnimatePresence>
         </div>
       </div>
     </Frame>
+  );
+}
+
+type ItemProps = {
+  item: { type: string };
+};
+function Item({ item }: ItemProps) {
+  const isPresent = useIsPresent();
+
+  useEffect(() => {
+    !isPresent && console.log("I've been removed!");
+  }, [isPresent]);
+
+  return (
+    <motion.div
+      key={item.type}
+      animate={{ scale: 1, transition: { duration: 0.3, ease: stepEase(4) } }}
+      exit={{
+        scale: 0,
+        transition: { duration: 0.3, ease: stepEase(3) },
+      }}
+      layout
+      className={styles.item}
+    >
+      [{isPresent ? " " : "x"}] {item.type}
+    </motion.div>
   );
 }
