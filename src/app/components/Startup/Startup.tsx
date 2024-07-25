c25d37f4304ee9fdb74db1f6710722366b2e16a6
Brandon Chang <brandonchang@stripe.com>
2024-07-24 23:45:07 -0400

    key fix

--- src/app/components/Startup/Startup.tsx
+++ src/app/components/Startup/Startup.tsx
@@ -11,32 +11,52 @@ export default function Startup() {
   const [cutoff, setCutoff] = useState(0);
 
   const lines = [
-    <div className={styles.line}>
+    <div key={1} className={styles.line}>
       <div className={styles.block} />
-      <div>STRIPE BIOS -- V42.42</div> 
+      <div>STRIPE BIOS -- V42.42</div>
       <div className={styles.block} />
     </div>,
-    <br/>,
-    <div><b>12 degrees POST driver</b></div>,
-    <br />,
-    <div>--- System health check start ---</div>,
-    <div className={styles.line}>Exothermic radiator<div className={styles.spacer} />[ OK ]</div>,
-    <div className={styles.line}>Feedback loop module<div className={styles.spacer} />[ SEEKING ]</div>,
-    <div className={styles.line}>User priority <div className={styles.spacer} />[ FIRST ]</div>,
-    <div className={styles.line}>Macros <div className={styles.spacer} />[ OPTIMIZED ]</div>,
-    <div className={styles.line}>Global financial infrastructure <div className={styles.spacer} />[ ONLINE ]</div>,
-    <div className={styles.line}>System resilience <div className={styles.spacer} />[ OK ]</div>,
-    <div className={styles.line}>Primary disk check <div className={styles.spacer} />[ OK ]</div>,
-    <div className={styles.line}>Display adapter <div className={styles.spacer} />[ OK ]</div>,
-    <div>--- System health check end ---</div>,
-    <br />,
-    <div>420Mhz clock rate</div>,
-    <div>RESOLUTION 420 x 640 (HI-RES)</div>,
-    <div className={styles.spacer} />,
-    <div>PRESS [42] to enter SETUP</div>,
-    <div>07/15/1984-WPP-BOOT-5.7</div>,
-    <div>Boot sequence start</div>,
-    <div>Price Adapter loading...</div>,
+    <br key={2} />,
+    <div key={3}>
+      <b>12 degrees POST driver</b>
+    </div>,
+    <br key={4} />,
+    <div key={5}>--- System health check start ---</div>,
+    <div key={6} className={styles.line}>
+      Exothermic radiator
+      <div className={styles.spacer} />[ OK ]
+    </div>,
+    <div key={7} className={styles.line}>
+      Feedback loop module
+      <div className={styles.spacer} />[ SEEKING ]
+    </div>,
+    <div key={8} className={styles.line}>
+      User priority <div className={styles.spacer} />[ FIRST ]
+    </div>,
+    <div key={9} className={styles.line}>
+      Macros <div className={styles.spacer} />[ OPTIMIZED ]
+    </div>,
+    <div key={10} className={styles.line}>
+      Global financial infrastructure <div className={styles.spacer} />[ ONLINE ]
+    </div>,
+    <div key={11} className={styles.line}>
+      System resilience <div className={styles.spacer} />[ OK ]
+    </div>,
+    <div key={12} className={styles.line}>
+      Primary disk check <div className={styles.spacer} />[ OK ]
+    </div>,
+    <div key={13} className={styles.line}>
+      Display adapter <div className={styles.spacer} />[ OK ]
+    </div>,
+    <div key={14}>--- System health check end ---</div>,
+    <br key={15} />,
+    <div key={16}>420Mhz clock rate</div>,
+    <div key={17}>RESOLUTION 420 x 640 (HI-RES)</div>,
+    <div key={18} className={styles.spacer} />,
+    <div key={19}>PRESS [42] to enter SETUP</div>,
+    <div key={20}>07/15/1984-WPP-BOOT-5.7</div>,
+    <div key={21}>Boot sequence start</div>,
+    <div key={22}>Price Adapter loading...</div>,
   ];
 
   useTransform(index, (latest) => {
@@ -58,9 +78,5 @@ export default function Startup() {
     };
   }, []);
 
-  return (
-    <div className={styles.container}>
-      {lines.slice(0, cutoff)}
-    </div>
-  );
+  return <div className={styles.container}>{lines.slice(0, cutoff)}</div>;
 }
