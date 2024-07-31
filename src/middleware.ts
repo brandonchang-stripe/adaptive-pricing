91c04a29edece68ae81ec07d527bc8ab01dcc1dd
Brandon Chang <brandonchang@stripe.com>
2024-07-31 12:39:17 -0400

    no favicon png middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,7 +39,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     {
-      source: "/((?!api|_next/static|_next/image|favicon.ico|sfx|sprites|audio).*)",
+      source: "/((?!api|_next/static|_next/image|favicon.png|sfx|sprites|audio).*)",
       missing: [
         { type: "header", key: "next-router-prefetch" },
         { type: "header", key: "purpose", value: "prefetch" },
